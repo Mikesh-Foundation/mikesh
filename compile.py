62b8eb82f0eaf21b2269bd0d252ad8c5069f87c7
Filip Brijar <filip.brijar@mikesh.science>
2021-09-27 20:02:22 +0200

    Compilation script now doesn't fail when used on Windows.

--- compile.py
+++ compile.py
@@ -1,4 +1,11 @@
-import shutil, os
+import shutil, os, platform
 
+#Determine operting system type
+op_system = platform.system()
+
+#Compile Rust code
 os.system("cargo build --release")
-shutil.move("./target/release/libmikesh.so", "./target/release/mikesh.so")
+
+#Rename library if on Linux
+if (op_system == "Linux"):
+	shutil.move("./target/release/libmikesh.so", "./target/release/mikesh.so")
